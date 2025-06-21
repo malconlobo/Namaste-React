d208c46baa078d0cc0026ff9f7822049fca11f43
Malcon lobo <malcon.lobo@sonetel.com>
2025-06-21 08:58:38 +0530

    Episode 2

--- namaste-react/App.js
+++ namaste-react/App.js
@@ -1,11 +1,14 @@
+import React from "react";
+import ReactDOM from "react-dom/client";
+
 const parent = React.createElement("div", { id: "parent" }, [
-  React.createElement("div", { id: "child" }, [
-    React.createElement("h1", {}, "I am h1 tag"),
-    React.createElement("h2", {}, "I am h2 tag"),
+  React.createElement("div", { id: "child" , key: "child1"}, [
+    React.createElement("h1", {key: "heading1"}, "I am h1 tag"),
+    React.createElement("h2", {key: "heading2"}, "I am h2 tag"),
   ]),
-  React.createElement("div", { id: "child2" }, [
-    React.createElement("h1", {}, "I am h1 tag"),
-    React.createElement("h2", {}, "I am h2 tag"),
+  React.createElement("div", { id: "child2" , key: "child2"}, [
+    React.createElement("h1", {key: "heading3"}, "I am h1 tag"),
+    React.createElement("h2", {key: "heading4"}, "I am h2 tag"),
   ]),
 ]);
 
