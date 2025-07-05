b561f734dc31abd7296970ba2c9307fc67b54850
Malcon lobo <malcon.lobo@sonetel.com>
2025-07-05 19:55:00 +0530

    Episode 3

--- namaste-react/App.js
+++ namaste-react/App.js
@@ -1,21 +1,38 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
 
-const parent = React.createElement("div", { id: "parent" }, [
-  React.createElement("div", { id: "child" , key: "child1"}, [
-    React.createElement("h1", {key: "heading1"}, "I am h1 tag"),
-    React.createElement("h2", {key: "heading2"}, "I am h2 tag"),
-  ]),
-  React.createElement("div", { id: "child2" , key: "child2"}, [
-    React.createElement("h1", {key: "heading3"}, "I am h1 tag"),
-    React.createElement("h2", {key: "heading4"}, "I am h2 tag"),
-  ]),
-]);
+//React.createElement => React element(JS Object) => render as HTML element
+// const heading = React.createElement(
+//   "h1",
+//   { id: "heading" },
+//   "Hello World from React"
+// );
 
-const heading = React.createElement(
-  "h1",
-  { id: "heading" },
-  "Hello World from React"
-);
+//JSX => Transpiled by Babel to React.createElement => React element(JS Object) => render as HTML element
+const heading = (<h1 id='heading'>Hello World from React using JSX</h1>)
+
+const TitleComponent = () => (
+  <h1 className="head" tabIndex="5">
+    Namaste React from TitleComponent
+  </h1>
+)
+
+const titleElement = (
+  <h1 className="head" tabIndex="5">
+    Namaste React from titleElement
+  </h1>
+)
+
+//Component composition 
+const HeadingComponent = () => (
+  <div id='container'>
+    <TitleComponent />
+    {titleElement}
+    <h1 id='heading'>Hello World from React using HeadingComponent</h1>
+  </div>
+)
+
+
+
 const root = ReactDOM.createRoot(document.getElementById("root"));
-root.render(parent);
+root.render(<HeadingComponent />);
